8a6edb8a57a0021a8b44d60a5b0eecfccc480433
A-shazli <Abdullahshazli18@gmail.com>
2022-05-24 01:09:50 +0200

    Updates

---
+++ precache-manifest.a8bc002400283a8f237ee8f52adbe859.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86d534f8cd2e78bb2478",
+    "url": "/VTK-JS/static/css/main.11738f0c.chunk.css"
+  },
+  {
+    "revision": "86d534f8cd2e78bb2478",
+    "url": "/VTK-JS/static/js/main.86d534f8.chunk.js"
+  },
+  {
+    "revision": "d6180efc8d39e1f4c0f6",
+    "url": "/VTK-JS/static/js/1.d6180efc.chunk.js"
+  },
+  {
+    "revision": "8cd78f9d3ebcb4169f40",
+    "url": "/VTK-JS/static/js/2.8cd78f9d.chunk.js"
+  },
+  {
+    "revision": "9952ed75e5413ef80fce",
+    "url": "/VTK-JS/static/js/runtime~main.9952ed75.js"
+  },
+  {
+    "revision": "70cf611fd19755e435d8da7d3744f3ca",
+    "url": "/VTK-JS/index.html"
+  }
+];
